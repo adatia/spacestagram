c555b8c2c278850d66e3674d7aefb2c0b2a7cd7e
adatias <shayn.adatia@gmail.com>
2022-01-09 17:24:55 -0500

    Added date picker

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import '@shopify/polaris/build/esm/styles.css';
+import en from '@shopify/polaris/locales/en.json';
+import { AppProvider, Page, Layout, Card } from '@shopify/polaris';
+import DateModal from './components/DateModal/DateModal';
+import Photo from './components/Photo/Photo';
+import axios from 'axios';
+import moment from 'moment';
 
 function App() {
+  const API_KEY = "fpJa8lCPaOjDFZcMdet9pXz3ohhkZ6ONlYDPmQvc";
+  const [selectedDates, setSelectedDates] = useState({
+    start: new Date(moment().subtract(1, 'months').format('ddd MMM DD YYYY')),
+    end: new Date(moment().format('ddd MMM DD YYYY')),
+  });
+
+  useEffect(() => {
+    console.log(selectedDates);
+  }, [selectedDates]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider i18n={en}>
+      <Page title="Spacestagram" subtitle="Brought to you by NASA's Astronomy Photo of the Day (APOD) API">
+        <Layout sectioned>
+          <Layout.Section>
+            <DateModal selectedDates={selectedDates} setSelectedDates={setSelectedDates}
+            />
+          </Layout.Section>
+          <Layout.Section>
+            <Photo />
+          </Layout.Section>
+        </Layout>
+      </Page>
+    </AppProvider>
   );
 }
 
