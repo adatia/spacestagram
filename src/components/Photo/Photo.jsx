8200c9a88d89b40d9ba302c098afbf5293d69bda
adatias <shayn.adatia@gmail.com>
2022-01-13 01:57:15 -0500

    Added heart icon

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { MediaCard } from '@shopify/polaris';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as fasHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons';
 
 function Photo({ post }) {
   const [liked, setLiked] = useState(false);
 
   return (
     <MediaCard title={post.title} primaryAction={{
+      icon: liked ? <FontAwesomeIcon icon={fasHeart} size='2x' color='red' transform="left-2" /> : <FontAwesomeIcon icon={farHeart} size='2x' color='red' transform="left-2" />,
       content: liked ? 'Unlike' : 'Like',
       onAction: () => {
         setLiked(!liked);
