4f3df46abfdd53b6818a4fb679dafb5689607fe2
adatias <shayn.adatia@gmail.com>
2022-01-14 18:42:40 -0500

    Added load more button and functionality

--- README.md
+++ README.md
@@ -11,12 +11,16 @@ With the help of NASA's Astronomy Photo of the Day (APOD) API, users can scroll 
 * [NASA Astronomy Photo of the Day (APOD) API](https://apod.nasa.gov/apod/astropix.html)
 
 # Features
-* Interface for viewing photos that were featured as NASA's Astronomy Photo of the Day, organized in three columns
-* Date selector to choose range of displayed photos
+* Feed of photos that were featured as NASA's Astronomy Photo of the Day, organized in three columns
+* Date selector to choose range of photos displayed in feed
+* "Load more" button to load additional posts; displayed when end of feed is reached
 * Pop-up modals to view full description and details of posts
 * Buttons for copying shareable image links to clipboard
-* Loading state while data is fetched from NASA's API
+* Loading state while data is fetched from NASA's API (initial data fetch + data fetches from "load more" button)
 * Animated "like" and "unlike" buttons
+
+# Other Notes
+* Occasionally, NASA uses a video instead of a photo for APOD. Days where a video is used instead of photo are excluded from the feed of posts on Spacestagram.
 
 # Usage
 Hosted live using GitHub Pages at [Spacestagram](https://adatias.github.io/spacestagram)
--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import '@shopify/polaris/build/esm/styles.css';
 import en from '@shopify/polaris/locales/en.json';
-import { AppProvider, Page, Layout, Spinner } from '@shopify/polaris';
+import { AppProvider, Page, Layout, Spinner, Button } from '@shopify/polaris';
 import DateModal from './components/DateModal/DateModal';
 import Photo from './components/Photo/Photo';
 import axios from 'axios';
@@ -16,6 +16,12 @@ function App() {
   });
   const [posts, setPosts] = useState([]);
   const [focusedPhoto, setFocusedPhoto] = useState('');
+  const [addedDates, setAddedDates] = useState({
+    startDate: moment().subtract(1, 'months').format('YYYY-MM-DD'),
+    endDate: moment().format('YYYY-MM-DD'),
+  });
+  const [loading, setLoading] = useState(false);
+  const [firstLoad, setFirstLoad] = useState(true);
 
   const getResults = async () => {
     const startDate = selectedDates.start.getFullYear() + '-' + (selectedDates.start.getMonth() + 1) + '-' + selectedDates.start.getDate();
@@ -37,13 +43,55 @@ function App() {
       });
 
       setPosts(results);
+      setAddedDates({
+        startDate: moment(selectedDates.start).format('YYYY-MM-DD'),
+        endDate: moment(selectedDates.end).format('YYYY-MM-DD'),
+      })
+      setFirstLoad(false);
     };
   }
 
   useEffect(() => {
+    setFirstLoad(true);
     setPosts([]);
     getResults();
   }, [selectedDates]);
+
+  const incrementDate = () => {
+    setLoading(true);
+    setAddedDates({
+      startDate: moment(addedDates.startDate).subtract(1, 'days').subtract(1, 'months').format('YYYY-MM-DD'),
+      endDate: moment(addedDates.startDate).subtract(1, 'days').format('YYYY-MM-DD'),
+    });
+  }
+
+  const loadMore = async () => {
+    const url = `https://api.nasa.gov/planetary/apod?start_date=${addedDates.startDate}&end_date=${addedDates.endDate}&api_key=${API_KEY}`;
+
+    const res = await axios.get(url);
+
+    if (res?.data) {
+      const results = res.data.reverse().map((item) => {
+        return {
+          title: item.title,
+          date: item.date,
+          description: item.explanation,
+          imageUrl: item.url,
+          image: item.media_type === 'image'
+        }
+      });
+
+      const concatResults = posts.concat(results);
+      setPosts(concatResults);
+      setLoading(false);
+    };
+  }
+
+  useEffect(() => {
+    if (!firstLoad) {
+      loadMore();
+    }
+  }, [addedDates]);
 
   return (
     <AppProvider i18n={en}>
@@ -54,22 +102,28 @@ function App() {
               (<div style={{ 'display': 'flex', 'justifyContent': 'center' }}>
                 <Spinner size="large" />
               </div>) :
-              (<DateModal selectedDates={selectedDates} setSelectedDates={setSelectedDates}
+              (<DateModal loading={loading} selectedDates={selectedDates} setSelectedDates={setSelectedDates}
               />)
             }
           </Layout.Section>
           <Layout.Section>
             <Layout>
-            {posts.map((post) => {
-              if (post.image) {
-                return (
-                  <Layout.Section oneThird>
-                    <Photo post={post} focusedPhoto={focusedPhoto} setFocusedPhoto={setFocusedPhoto} />
-                  </Layout.Section>
-                );
-              }
-            })}
+              {posts.map((post) => {
+                if (post.image) {
+                  return (
+                    <Layout.Section oneThird>
+                      <Photo post={post} focusedPhoto={focusedPhoto} setFocusedPhoto={setFocusedPhoto} />
+                    </Layout.Section>
+                  );
+                }
+              })}
             </Layout>
+          </Layout.Section>
+          <Layout.Section>
+            {posts.length === 0 ? null :
+              (<div style={{ 'display': 'flex', 'justifyContent': 'center' }}>
+                <Button primary loading={loading} onClick={incrementDate}>Load more</Button>
+              </div>)}
           </Layout.Section>
         </Layout>
       </Page>
